88b92ead67c7e42b3606f6ed435c1d207278e92c
Jose <Jose@JOSEPH>
2017-02-12 16:01:27 -0500

    actualizar index

--- index.php
+++ index.php
@@ -11,15 +11,15 @@ function p( $resultado, $title = '' ){
 
 $db = Database::getInstance();
 
-//$userId = $db->create( 'usuarios', array('username' => 'Maria', 'password' => md5( 'maria' ), 'email' => 'maria@mail.com' ) );
-//
-//$user = $db->read( 'usuarios', array( 'id' => $userId ) );
-//
-//p($user, 'Read');
-//
-//$useru = $db->update( 'usuarios', ['username' => 'Laura'] , [ 'id' => $userId] );
-//echo '<h2>Update</h2>';
-//var_dump($useru);
+$userId = $db->create( 'usuarios', array('username' => 'Maria', 'password' => md5( 'maria' ), 'email' => 'maria@mail.com' ) );
+
+$user = $db->read( 'usuarios', array( 'id' => $userId ) );
+
+p($user, 'Read');
+
+$useru = $db->update( 'usuarios', ['username' => 'Laura'] , [ 'id' => $userId] );
+echo '<h2>Update</h2>';
+var_dump($useru);
 
 $userd = $db->delete( 'usuarios', [ 'id' => 26] );
 echo '<h2>Delete</h2>';
